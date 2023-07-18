5055c340601b026cd743125574901a68f66e6ce3
Streamrivulet <jfranamoh@gmail.com>
2023-07-18 22:52:13 +0100

    removed format

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 word = "School"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print("First 3 letters: {}".format(Hol))
-print("Last 2 letters: {}".format(on))
-print("Middle word: {}".format(olberto))
+print("First 3 letters: Hol")
+print("Last 2 letters: on")
+print("Middle word: olberto")
 print("First 3 letters: "  + word[0:3])
 print("Last 2 letters: " + word[4:])
 print("Middle word: " + word[1:5])
