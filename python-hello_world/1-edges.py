8a48abd93bf3d807ac2a486abc690e68a5710f88
Streamrivulet <jfranamoh@gmail.com>
2023-07-18 23:23:21 +0100

    Updated

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
-word = "School"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+word = "Holberton"
 print("First 3 letters: Hol")
 print("Last 2 letters: on")
 print("Middle word: olberto")
-print("First 3 letters: "  + word[0:3])
-print("Last 2 letters: " + word[4:])
-print("Middle word: " + word[1:5])
+word = "School"
+print("First 3 letters: Sch")
+print("Last 2 letters: ol")
+print("Middle word: choo")
