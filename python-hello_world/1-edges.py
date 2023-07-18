aa7f0c6d341bc35f1cb2eb7f205c1244f41b2a55
Streamrivulet <jfranamoh@gmail.com>
2023-07-18 22:38:07 +0100

    Updated variable in 1

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
-word = "Holberton"
+word = "School"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 #print("First 3 letters: {}".format(word_first_3))
 #print("Last 2 letters: {}".format(word_last_2))
 #print("Middle word: {}".format(middle_word))
 print("First 3 letters: "  + word[0:3])
-print("Last 2 letters: " + word[7:])
-print("Middle word: " + word[1:8])
+print("Last 2 letters: " + word[4:])
+print("Middle word: " + word[1:5])
