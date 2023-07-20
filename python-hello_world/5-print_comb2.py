d12263b0e31c941a0151c14fb0a7ee3b6c7de51d
Streamrivulet <jfranamoh@gmail.com>
2023-07-20 17:26:40 +0100

    Added if

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,2 +1,12 @@
 for i in range(100):
-    print("{:02}" .format(i), end=", ")
+         
+    if i == 99:
+        print(i)
+
+    else:
+         print("{:02}" .format(i), end=", ")
+
+
+
+        
+ 
