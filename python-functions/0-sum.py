84dfbfc358ddb2c23272ec0c1fa2868bfff4064c
Streamrivulet <jfranamoh@gmail.com>
2023-07-25 15:37:45 +0100

    chandw

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,7 +1,8 @@
 def add(a,b):
-    print(add(1,2))
-    print(add(100,-2))
-    print(add(-100,-2))
-    print(add(0,0))
+    return a+b
+print(add(1,2))
+print(add(100,-2))
+print(add(-100,-2))
+print(add(0,0))
 
-    return a + b
+    
