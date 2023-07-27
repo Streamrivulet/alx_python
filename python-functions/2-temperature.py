19266105f768f539a1e308149558954e4ba30cc5
Streamrivulet <jfranamoh@gmail.com>
2023-07-27 11:33:58 +0100

    to 2 decimal

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,5 +1,4 @@
 def convert_to_celcius(fahrenheit):
-    celcius = ((fahrenheit - 32.0) * 5.0) / 9.0
-    return celcius
+    return ((fahrenheit-32.00)*(5/9))
 
     #return ((int(fahrenheit) - 32) * 5) /9
