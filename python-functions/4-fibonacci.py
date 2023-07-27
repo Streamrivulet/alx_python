ac9b25ff7bd247a9bbe99ea70d5875a84cba18f2
Streamrivulet <jfranamoh@gmail.com>
2023-07-27 14:39:54 +0100

    using for

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,15 +1,5 @@
 def fibonacci_sequence(n):
-    num1 = 0
-    num2 = 1
-    next_number = 0
-    count = 1
-
-    while(count <= n):
-        print(next_number, end=" ")
-        count += 1
-        num1 = num2
-        num2 = next_number
-        next_number = num1 + num2
-    t_number = num1 + num2
-
-    return t_number
+    for i in n:
+        i < n
+        n = n + n
+    return n
