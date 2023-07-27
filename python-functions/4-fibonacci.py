f0e130db07056ec41afb7d8968e490b6f229d43d
Streamrivulet <jfranamoh@gmail.com>
2023-07-27 18:24:37 +0100

    Fibo

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,10 +1,21 @@
 def fibonacci_sequence(n):
-   while n >= 1:
-         n = ((n-1) + (n-2))
-         while n <= n:
-             n +=n
+    first = 0
+    second = 1
+    print(first, second, end=" ")
+
+    n -= 2
+    
+    while n > 0:
+
+        print(first + second, end=" ")
+
+        temp = second
+        second = first + second
+        first = temp
+
+    
+        n -= 1
       
-      
-   return [n]
+    return [n]
 
 
