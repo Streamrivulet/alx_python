d04eee2570ff391eee1d409b8ceeccf63f2f706a
Streamrivulet <jfranamoh@gmail.com>
2023-07-27 14:09:47 +0100

    using while

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,11 +1,16 @@
 def fibonacci_sequence(n):
-   
-	if n < 0:
-		print("Incorrect input")
-	elif n == 0:
-		return 0
-	elif n == 1 or n == 2:
-		return 1
+    n = int(input("Enter length of Fibonacci series: "))
+    num1 = 0
+    num2 = 1
+    next_number = 0
+    count = 1
 
-	else:
-		return fibonacci_sequence(n-1) + fibonacci_sequence(n-2)
+    while(count <= n):
+        print(next_number, end=" ")
+        count += 1
+        num1 = num2
+        num2 = next_number
+        next_number = num1 + num2
+    t_number = num1 + num2
+
+    return t_number
