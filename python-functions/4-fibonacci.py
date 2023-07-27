bb1d2274adb1521ed39cec2905c1252cdf5527d8
Streamrivulet <jfranamoh@gmail.com>
2023-07-27 14:47:54 +0100

    Using if again

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,5 +1,5 @@
 def fibonacci_sequence(n):
-    for i in n:
-        i < n
-        n = n + n
-    return n
+   if n <= 1:
+       return (n)
+   else:
+       return (fibonacci_sequence(n-1) + fibonacci_sequence(n-2))
