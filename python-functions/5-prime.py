a83d8e8524125dfbfaaed4c5c99c5fcf0685383a
Streamrivulet <jfranamoh@gmail.com>
2023-07-28 17:29:32 +0100

    Added just one not condition

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -2,7 +2,7 @@
 def is_prime(number):
 	if (number/number == 1) & (number/1 == number):
 		return True
-	if (number/number != 1) & (number/1 != number):
+	if (number/number) != 1:
 		return False
 #def Prime(number,itr): #prime function to check given number prime or not
 #if itr == 1: #base condition
