9f003aae640a21b91d2e687f8d276ba84e1b1241
Streamrivulet <jfranamoh@gmail.com>
2023-07-28 17:46:22 +0100

    Added not

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,5 +1,5 @@
 def is_prime(number):
 	if ((number/number) == 1) & ((number/1) == number):
 		return True
-	if ((number/number) == 1) & ((number/1) == number):
+	if ((number/number) != 1) & ((number/1) != number):
 		return False
