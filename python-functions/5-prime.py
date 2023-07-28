0cd957182701949ea38434818a9b0298be788732
Streamrivulet <jfranamoh@gmail.com>
2023-07-28 17:04:57 +0100

    prime

---
+++ python-functions/5-prime.py
@@ -0,0 +1,22 @@
+#from math import sqrt
+def is_prime(number):
+	i= input("Input number")
+	if i/i ==1 & i/1 == i:
+		return True
+	if number % i == 0:
+		return False
+#def Prime(number,itr): #prime function to check given number prime or not
+#if itr == 1: #base condition
+#	return True
+#if number % itr == 0: #if given number divided by itr or not
+#	return False
+#if Prime(number,itr-1) == False: #Recursive function Call
+#	return False
+	
+#return True
+
+#num = 13
+
+#itr = int(sqrt(num)+1)
+
+#print(Prime(num,itr))
