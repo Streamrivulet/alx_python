d2a2b7e13450ace5e74c3b787e63f51a743153ce
Streamrivulet <jfranamoh@gmail.com>
2023-07-30 11:37:20 +0100

    Added 3rd if

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,6 +1,6 @@
 def is_prime(number):
 	if number <= 3:
-        return number > 1
+		return number>1
 	if ((number/number) == 1) | ((number/1) == number):
 		return True
 	if ((number/number) != 1) | ((number/1) != number):
