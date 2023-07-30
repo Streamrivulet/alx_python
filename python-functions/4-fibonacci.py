bd3d06e75c465350535e95a27c95410ca8eb8962
Streamrivulet <jfranamoh@gmail.com>
2023-07-30 10:56:38 +0100

    Using array and append

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,23 +1,10 @@
 def fibonacci_sequence(n):
-    first[0]=0
+    if n <= 0:
+        return []
 
-    second[1]=[1]
+    array = [0, 1]
 
-    print(first, second, end=" ")
-
-    n -= 2
-    
-    while n > 0:
-        second[n]={ first[-2] + second[-1]} 
-
-        temp = second
-        second = first + second
-        first = temp
-
-        print(first[n] + second[n], end=" ")
-    
-        n -= 1
-      
-        return [n]
-
-
+    while len(array) < n:
+        next = array[-1] + array[-2]
+        array.append(next)
+    return array[:n] 
