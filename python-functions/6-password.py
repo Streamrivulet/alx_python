9b001372baf1a2143a94715d7026a1d0d9da1d7e
Streamrivulet <jfranamoh@gmail.com>
2023-07-30 11:09:02 +0100

    Password validation

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,8 +1,5 @@
 def validate_password(password):
-	if ((len(password) >= 8) &  (any(char.islower() for char in password)) & (any(char.isupper() for char in password)) & isspace() for char not in password))):
+	if ((len(password) >= 8) &  (any(char.islower() for char in password)) & (any(char.isupper() for char in password))):
 		return True
 	else:
 		return False
-	
-		
-#
