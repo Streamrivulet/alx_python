e0388847eac4279ea6c386fe9fc431d83fff7fce
Streamrivulet <jfranamoh@gmail.com>
2023-07-31 22:08:21 +0100

    Added if

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,10 @@
-__import__("add_0")
 from add_0 import add
-a = 1
-b = 2
-add(a, b)
+if add=="main":
+    a = 1
+    b = 2
+    add(a, b)
 
-print("{} + {} = {}" .format(a, b, add(a, b)), end="\n")
+    print("{} + {} = {}" .format(a, b, add(a, b)), end="\n")
 
 
 
