110054a5e4e0454a41e1f8a1853c26f677b6734f
Streamrivulet <jfranamoh@gmail.com>
2023-08-02 23:08:16 +0100

    Zerodivisor error

---
+++ python-classes/README.md
@@ -0,0 +1,1 @@
+This is my ALX classes and objects code directory 
--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,8 +1,12 @@
 def safe_print_division(a, b):
-
     try:
         result = a/b
         print("{}/{} = {}" .format(a,b, result)) 
-    except:
+
+    except: ZeroDivisionError
+    print("you cant divide by zero")
+    
+    finally: 
+         print("An error occur")
         
     
