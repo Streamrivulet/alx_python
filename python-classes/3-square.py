f6f933904f5d2031a7a182c8cff9482a1e60176a
Streamrivulet <jfranamoh@gmail.com>
2023-08-03 14:24:08 +0100

    Without setter

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,5 +1,5 @@
 """
-Alx task 1,m  on classes and object 
+Alx task 1 on classes and object 
 """
 
 """
@@ -7,41 +7,24 @@ Delaring the class
 """
 class Square:
    """
-
    Delaring the class
    """
-   __size = ""
-   #def __init__(self, size = 0):
-       #self.__size = size
-
-   @property
-   def size(self):
-        """
-        Adding property in order to retrieve private attribute size
-        """
-        return self.__size
-   size.setter
-
-   """
-   Adding setter in order to update the size retrieved
-   """
-   def size(self, value):
-       """ 
-       #Raising and handling some exceptions
-       """
-       if type(value) is not int:
-           raise TypeError("size must be an integer")
-       elif value < 0:
-           raise ValueError("size must be >= 0")
-       else:
-           self.__size = value
-
-   def __init__(self, size = 0):
-       self.__size = size
-
+   def __init__(self, size = 0): #Assigned a default value to size
+      
+      """
+      Raising and handling some exceptions
+      """
+      if type(size) is not int:
+         raise TypeError("size must be an integer")
+      elif size < 0:
+         raise ValueError("size must be >= 0")
+      else:
+        self.__size = size
 
    def area(self):
         """
-         #Creating another method that calculate and return the area of the square.
+         Creating another method that calculate and return the area of the square.
         """
-        return self.size** 2
+        return self.__size*self.__size
+    
+    
