34b3dad32fd34b38ad99977910fdc1fe5dc05ee9
Streamrivulet <jfranamoh@gmail.com>
2023-08-03 14:46:17 +0100

    Indentation

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,30 +1,28 @@
-"""
-Alx task 1 on classes and object 
-"""
-
-"""
-Delaring the class
-"""
 class Square:
-   """
-   Delaring the class
-   """
-   def __init__(self, size = 0): #Assigned a default value to size
+    
+    def __init__(self, size):
+       self.__size = size 
+    
+       @property
+       def size(self):
+        return self.__size
+       
+       @size.setter
+       def size(self, value):
+          if type(value) is not int:
+             raise TypeError("size must be an integer")
+          elif value < 0:
+             raise ValueError("size must be >= 0")
+          else:
+            self.__size = value
+    
+    def __init__(self, size = 0): #Assigned a default value to size
+       self.__size = size 
       
-      """
-      Raising and handling some exceptions
-      """
-      if type(size) is not int:
-         raise TypeError("size must be an integer")
-      elif size < 0:
-         raise ValueError("size must be >= 0")
-      else:
-        self.__size = size
 
-   def area(self):
+    def area(self):
         """
          Creating another method that calculate and return the area of the square.
         """
         return self.__size*self.__size
-    
     
