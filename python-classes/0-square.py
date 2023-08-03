e43acbfb7ab0aec87a4eb19e56c7f2600829b78c
Streamrivulet <jfranamoh@gmail.com>
2023-08-03 12:11:38 +0100

    Alx task O on classes and object

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,12 @@
+"""
+Alx task O on classes and object 
+"""
+
+"""
+Delaring the class
+"""
 class Square:
-   def __size():
-      size = 2
+
+   def __init__(self, size):
+      self.__size = size
       
