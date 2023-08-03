9094f0da5f4292244326b8ba8c2b886b94af570c
Streamrivulet <jfranamoh@gmail.com>
2023-08-03 12:15:48 +0100

    Added documentation to the class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -6,7 +6,10 @@ Alx task O on classes and object
 Delaring the class
 """
 class Square:
-
+   """
+   Delaring the class
+   """
+   
    def __init__(self, size):
       self.__size = size
       
