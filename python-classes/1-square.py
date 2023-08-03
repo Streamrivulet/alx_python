cb42a3e2cd9a52f9a6f5ae2ec0b18383560518ea
Streamrivulet <jfranamoh@gmail.com>
2023-08-03 12:29:33 +0100

    Raising and handling some exceptions

---
+++ python-classes/1-square.py
@@ -0,0 +1,24 @@
+"""
+Alx task 1 on classes and object 
+"""
+
+"""
+Delaring the class
+"""
+class Square:
+   """
+   Delaring the class
+   """
+   
+   def __init__(self, size = 0): #Assigned a default value to size
+      
+      """
+      Raising and handling some exceptions
+      """
+      if type(size) is not int:
+         raise TypeError("size must be an integer")
+      elif size < 0:
+         raise ValueError("size must be >= 0")
+      else:
+        self.__size = size
+      
